9593a3a8eec225290b3c5f62b8932c2451e0f2b3
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-03 22:28:16 -0300

    define relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Image;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
