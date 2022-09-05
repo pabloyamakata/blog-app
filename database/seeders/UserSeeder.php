092253cc422a2150b05c62e2ba248fa154074137
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-05 20:55:24 -0300

    create seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Pablo Yamakata',
+            'email' => 'pablo.yamakata@gmail.com',
+            'password' => password_hash('blog', PASSWORD_BCRYPT)
+        ]);
+
+        User::factory(19)->create();
+    }
+}
