ecbdd84395ce3f5c9b07f733dc0c68dc18c77903
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-06 17:13:56 -0300

    remove storage facade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,6 @@ use App\Models\Tag;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\PostSeeder;
-use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,9 +19,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Storage::deleteDirectory('posts');
-        Storage::makeDirectory('posts');
-
         $this->call(UserSeeder::class);
         Category::factory(4)->create();
         Tag::factory(6)->create();
