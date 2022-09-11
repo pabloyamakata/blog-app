6c41393b60c760c216d3e60fa4ec1139409c43f8
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-11 19:56:40 -0300

    add methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
 }
