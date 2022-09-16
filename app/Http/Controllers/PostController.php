595346e00b165166f8efbfdefdf3df6a45190908
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-16 17:24:24 -0300

    feat: show specific post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,16 @@ class PostController extends Controller
 
         return view('posts.index', compact('posts'));
     }
+
+    public function show(Post $post)
+    {
+        $relatedPosts = Post::where('status', 'published')
+                            ->where('category_id', $post->category_id)
+                            ->where('id', '!=', $post->id)
+                            ->latest('id')
+                            ->take(4)
+                            ->get();
+        
+        return view('posts.show', compact('post', 'relatedPosts'));
+    }
 }
