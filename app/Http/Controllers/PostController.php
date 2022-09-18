6740d9a613ba416bc4768f88c1542baf4b699a86
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-18 18:56:30 -0300

    add category method
    Get posts based on category and return view file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -24,5 +25,15 @@ class PostController extends Controller
                             ->get();
         
         return view('posts.show', compact('post', 'relatedPosts'));
+    }
+
+    public function category(Category $category)
+    {
+        $posts = Post::where('category_id', $category->id)
+                     ->where('status', 'published')
+                     ->latest('id')
+                     ->paginate(5);
+
+        return view('posts.category', compact('posts', 'category'));
     }
 }
