d861537086c5021d593b7168ee7e7f921f07c987
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-20 19:22:02 -0300

    add route to posts.tag

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -8,7 +8,7 @@
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->extract }}</p>
         @foreach($post->tags as $tag)
-            <a href="#" class="inline-flex items-center mr-2 py-1 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            <a href="{{ route('posts.tag', $tag) }}" class="inline-flex items-center mr-2 py-1 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                 {{ $tag->name }}
             </a>
         @endforeach
