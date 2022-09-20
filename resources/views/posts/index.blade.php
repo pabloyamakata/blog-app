3882b86cc2f5d9d76cbad1fa92552c0ccf26234f
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-20 19:50:44 -0300

    add route to posts.tag

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,10 +9,10 @@
                     <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div>
                             @foreach($post->tags as $tag)
-                                <a href="" class="inline-block px-3 h-6 bg-{{ $tag->color }}-600 text-white rounded-full">{{ $tag->name }}</a>
+                                <a href="{{ route('posts.tag', $tag) }}" class="inline-block px-3 h-6 bg-{{ $tag->color }}-600 text-white rounded-full">{{ $tag->name }}</a>
                             @endforeach
                         </div>
-                        <h2 class="text-4xl text-white leading-8 font-bold">
+                        <h2 class="text-4xl text-white leading-8 font-bold pt-2">
                             <a href="{{ route('posts.show', $post) }}">{{ $post->name }}</a>
                         </h2>
                     </div>
