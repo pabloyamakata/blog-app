6db0350beb336a2e22acfe5e6876b3b0d6e1cd58
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-20 16:50:38 -0300

    add title to the page

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -3,7 +3,12 @@
     <x-navbar />
 
     <div class="container mx-auto px-2 py-10">
-        <div class="md:grid-cols-2 lg:grid-cols-3 grid justify-items-center grid-cols-1 gap-6">
+
+        <h2 class="text-4xl text-gray-600 text-center">
+            Category: {{ $category->name }}
+        </h2>
+
+        <div class="md:grid-cols-2 lg:grid-cols-3 grid justify-items-center grid-cols-1 gap-6 pt-6">
             @foreach($posts as $post)
                 <article class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
                     <a href="{{ route('posts.show', $post) }}">
