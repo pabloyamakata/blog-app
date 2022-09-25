a11dd5b3598cf802c92914a28a5c810f4b884132
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-24 21:48:46 -0300

    create resource route

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('admin.index');
+Route::resource('categories', CategoryController::class)->names('admin.categories');
