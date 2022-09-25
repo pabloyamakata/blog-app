b6cd74a78f191d6c6687e598df2dcab947a02b7f
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-09-24 21:52:29 -0300

    create mutator to slug category name

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'slug'];
+
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    protected function slug(): Attribute
+    {
+        return Attribute::make(
+            set: fn($name) => Str::slug($name)
+        );
     }
 }
