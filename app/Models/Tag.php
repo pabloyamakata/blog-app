cc9000ec2135b99f7cd1a0031d2002a557e903f3
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-04 19:33:01 -0300

    add mutator to create slug

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
@@ -13,5 +15,12 @@ class Tag extends Model
     public function posts()
     {
         return $this->belongsToMany(Post::class);
+    }
+
+    protected function slug(): Attribute
+    {
+        return Attribute::make(
+            set: fn($name) => Str::slug($name)
+        );
     }
 }
