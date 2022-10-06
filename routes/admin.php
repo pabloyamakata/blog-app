bf91b308e7c7464b98afe6a670b8021c3a36ba17
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-05 21:47:54 -0300

    add resource route

--- routes/admin.php
+++ routes/admin.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\PostController;
 use App\Http\Controllers\admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('admin.index');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 Route::resource('tags', TagController::class)->names('admin.tags');
+Route::resource('posts', PostController::class)->names('admin.posts');
