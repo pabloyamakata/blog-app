28b854ac921ceb3c2d56e5dc1b866fed64b2209c
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-05 21:42:21 -0300

    add new section to sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -329,59 +329,16 @@ return [
             'icon' => 'fas fa-fw fa-bookmark',
             'active' => ['admin/tags*']
         ],
+        ['header' => 'BLOG OPTIONS'],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text' => 'Posts',
+            'route' => 'admin.posts.index',
+            'icon' => 'fas fa-fw fa-clipboard'
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text' => 'New Post',
+            'route' => 'admin.posts.create',
+            'icon' => 'fas fa-fw fa-file'
         ],
     ],
 
