35469cb019a109381fb39404d91d5cc4a84ac520
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-21 17:21:36 -0300

    use special syntax to display unescaped data

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,7 +6,7 @@
         <a href="{{ route('posts.show', $post) }}">
             <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->name }}</h3>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->extract }}</p>
+        <div class="mb-3 font-normal text-gray-700">{!! $post->extract !!}</div>
         @foreach($post->tags as $tag)
             <a href="{{ route('posts.tag', $tag) }}" class="inline-flex items-center mr-2 py-1 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                 {{ $tag->name }}
