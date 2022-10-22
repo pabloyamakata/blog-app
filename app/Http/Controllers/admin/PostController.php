4e65d0d6745903806ca911a4e51bdd8c2e48a987
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-21 21:24:53 -0300

    feat: handle file upload

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -8,6 +8,7 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
 use App\Http\Requests\StorePost;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -53,6 +54,13 @@ class PostController extends Controller
             'category_id' => $request->category,
             'user_id' => $request->user_id
         ]);
+
+        if($request->file('file')) {
+            $url = Storage::put('public/posts', $request->file('file'));
+            $post->image()->create([
+                'url' => $url
+            ]);
+        }
 
         if($request->tags) {
             $post->tags()->attach($request->tags);
