5db48310758a7685a423272c07a44541d2c30f96
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-28 22:51:48 -0300

    give permission to handle users, categories and tags only to admin

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -21,15 +21,19 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name' => 'admin.index'])->syncRoles([$admin, $blogger]);
 
-        Permission::create(['name' => 'admin.categories.index'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.categories.create'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.categories.edit'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.categories.destroy'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.users.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.users.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.users.update'])->syncRoles([$admin]);
 
-        Permission::create(['name' => 'admin.tags.index'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.tags.create'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.tags.edit'])->syncRoles([$admin, $blogger]);
-        Permission::create(['name' => 'admin.tags.destroy'])->syncRoles([$admin, $blogger]);
+        Permission::create(['name' => 'admin.categories.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.categories.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.categories.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.categories.destroy'])->syncRoles([$admin]);
+
+        Permission::create(['name' => 'admin.tags.index'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.tags.create'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.tags.edit'])->syncRoles([$admin]);
+        Permission::create(['name' => 'admin.tags.destroy'])->syncRoles([$admin]);
 
         Permission::create(['name' => 'admin.posts.index'])->syncRoles([$admin, $blogger]);
         Permission::create(['name' => 'admin.posts.create'])->syncRoles([$admin, $blogger]);
