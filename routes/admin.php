7cc5ea9468ab0ac1470f497a854a3d65b9eec774
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-28 22:43:51 -0300

    add resource route to handle user crud

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\PostController;
 use App\Http\Controllers\admin\TagController;
+use App\Http\Controllers\admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('admin.index');
+Route::resource('users', UserController::class)->only(['index', 'edit', 'update'])->names('admin.users');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 Route::resource('tags', TagController::class)->names('admin.tags');
 Route::resource('posts', PostController::class)->names('admin.posts');
