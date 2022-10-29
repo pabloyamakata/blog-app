8f450b3a617e8058e90cb334c7eb15799a7c5a6f
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-10-28 22:45:05 -0300

    add controller to handle user crud

---
+++ app/Http/Controllers/admin/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate();
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $roles = Role::all();
+        $userRoles = $user->roles()->pluck('id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles', 'userRoles'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.edit', $user)
+                         ->with('update-user-success', 'Record was updated successfully');
+    }
+}
