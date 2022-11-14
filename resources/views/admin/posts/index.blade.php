a14eada2d6c59954b6f4935133dfc65b5a6b4795
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-13 23:45:27 -0300

    add livewire component to list current user posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -14,39 +14,10 @@
         </div>
     @endif
 
-    <div class="card">
-        <div class="card-body">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th colspan="2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($posts as $post)
-                        <tr>
-                            <td>{{ $post->id }}</td>
-                            <td>{{ $post->name }}</td>
-                            <td style="width: 10px">
-                                <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-primary btn-sm">Edit</a>
-                            </td>
-                            <td style="width: 10px">
-                                <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+    <livewire:admin.post-index />
 
-        <div class="card-footer">
-            {{ $posts->links('pagination::bootstrap-5') }}
-        </div>
-    </div>
+@stop
+
+@section('js')
+    @livewireScripts
 @stop
