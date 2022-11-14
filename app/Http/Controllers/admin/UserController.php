66f9ab016264bbb1d1e7f5dcc24853248b1994b4
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-14 01:04:33 -0300

    remove users var

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -22,9 +22,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate();
-
-        return view('admin.users.index', compact('users'));
+        return view('admin.users.index');
     }
 
     /**
