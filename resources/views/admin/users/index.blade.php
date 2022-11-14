8023fec3a39d460094d0bc7857c297dc0c41dc56
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-14 01:07:09 -0300

    include livewire component to list all users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,34 +8,10 @@
 
 @section('content')
 
-    <div class="card">
-        <div class="card-body">
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($users as $user)
-                        <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td style="width: 10px">
-                                <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-primary btn-sm">Edit</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+    <livewire:admin.user-index />
 
-        <div class="card-footer">
-            {{ $users->links('pagination::bootstrap-5') }}
-        </div>
-    </div>
+@stop
+
+@section('js')
+    @livewireScripts
 @stop
