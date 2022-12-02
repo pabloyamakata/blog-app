7b5bfb3a37ac253217961e92f3fb1b3c496c2b31
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-12-02 16:01:00 -0300

    style component

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,31 +1,32 @@
 <div class="overflow-hidden bg-white shadow sm:rounded-lg">
-    <div class="px-4 py-5 sm:px-6">
-        <h3 class="text-lg font-medium leading-6 text-gray-900">Your Profile</h3>
-        <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal information</p>
+    <div class="flex justify-between flex-wrap px-4 py-5 sm:px-6">
+        <div>
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Your Profile</h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal information</p>
+        </div>
+        <a href="{{ route('profile.edit', $user) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit Profile</a>
     </div>
        
     <div class="border-t border-gray-200">
         <dl>
+            
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Username</dt>
-                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ auth()->user()->name }}</dd>
+                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ $user->name }}</dd>
             </div>
+
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Email address</dt>
-                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ auth()->user()->email }}</dd>
+                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ $user->email }}</dd>
             </div>
+
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Roles</dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    @if(auth()->user()->getRoleNames()->count() == 1)
-                        {{ auth()->user()->getRoleNames()[0] }}
-                    @elseif(auth()->user()->getRoleNames()->count() > 1)
-                        {{ auth()->user()->getRoleNames()->implode(', ') }}
-                    @else
-                        {{ 'None' }}
-                    @endif
+                    {{ $roles }}
                 </dd>
             </div>
+
         </dl>
     </div>
 </div>
